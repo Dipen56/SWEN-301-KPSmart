678c62235c444f89d7aff0e66002bad45cf5df0a
Dipen <pateldipe1@ecs.vuw.ac.nz>
2017-04-18 15:41:39 +1200

    gui class created

---
+++ KPS Software/src/View/GUI.java
@@ -0,0 +1,19 @@
+package View;/**
+ * Created by Dipen on 18/04/2017.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.show();
+
+    }
+}

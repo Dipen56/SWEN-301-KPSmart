ac31afa2d649b354290ca39d786ef9a74c67348d
Dipen <pateldipe1@ecs.vuw.ac.nz>
2017-04-19 00:38:14 +1200

    backing up

--- KPS Software/src/View/GUI.java
+++ KPS Software/src/View/GUI.java
@@ -6,6 +6,10 @@ import Controller.GUIController;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
 
 public class GUI extends Application {
     private static GUIController controller;
@@ -23,6 +27,14 @@ public class GUI extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Kelburn Postal Smart - Buttercup");
         primaryStage.getIcons().add(LoadResources.ICON_IMAGE);
+        try {
+            BorderPane page = (BorderPane) FXMLLoader.load(GUI.class.getResource("/rec/login screen.fxml"));
+            Scene scene = new Scene(page);
+            primaryStage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         primaryStage.show();
 
     }

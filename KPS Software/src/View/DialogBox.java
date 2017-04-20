dc56b24ea5d0e31140e73c59541877a72e1edcd3
Dipen <pateldipe1@ecs.vuw.ac.nz>
2017-04-21 11:12:23 +1200

    connecting screens

---
+++ KPS Software/src/View/DialogBox.java
@@ -0,0 +1,7 @@
+package View;
+
+/**
+ * Created by Dipen on 21/04/2017. This class is a will only have static method used for dialog box.
+ */
+public class DialogBox {
+}
--- KPS Software/src/View/GUI.java
+++ KPS Software/src/View/GUI.java
@@ -14,29 +14,32 @@ import java.io.IOException;
 
 public class GUI extends Application {
     private static GUIController controller;
-    Scene loginScene;
+    private static LoginScreen loginScene;
+    private static Stage primaryStage;
 
-    public  GUI(GUIController controller){
+    public GUI(GUIController controller) {
         this.controller = controller;
 
+
     }
+
     public GUI() {
 
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) {
+        primaryStage = stage;
         primaryStage.setTitle("Kelburn Postal Smart - Team Buttercup");
         primaryStage.getIcons().add(LoadResources.ICON_IMAGE);
         primaryStage.setResizable(false);
-        try {
-            HBox page = (HBox) FXMLLoader.load(GUI.class.getResource("/rec/home screen.fxml"));
-            Scene scene = new Scene(page);
-            primaryStage.setScene(scene);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        displayLoginScreen();
         primaryStage.show();
-
     }
+
+    public void displayLoginScreen() {
+        loginScene = new LoginScreen();
+        primaryStage.setScene(loginScene.getScene());
+    }
+
 }

b2a616c980ea6b2e5ba172ef67b73810eae33bcb
patevs <ppevans11@gmail.com>
2017-04-20 15:52:03 +1200

    Added Staff.java abstract class in the Model.User package

---
+++ KPS Software/src/Model/User/Staff.java
@@ -0,0 +1,56 @@
+package Model.User;
+
+public abstract class Staff {
+	
+	private int UID;
+	private String userName;
+	private String password;
+	
+	public Staff(int uid, String name, String pass){
+		this.setUID(uid);
+		this.setUserName(name);
+		this.setPassword(pass);
+	}
+	
+	/**
+	 * This methods checks if a given login user name and password match
+	 * 	those of this staff member. Returns true if both match and false 
+	 * 	if either does not match.
+	 * 
+	 * @param name
+	 * @param pass
+	 * @return
+	 */
+	public boolean checkLogin(String name, String pass){
+		if(this.userName.equals(name) 
+				&& this.password.equals(pass)){
+			return true;
+		}
+		return false;
+	}
+
+	
+	/*
+	 * Getter and Setter methods.
+	 * 
+	 */
+	public int getUID() {
+		return UID;
+	}
+	public void setUID(int uID) {
+		UID = uID;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}

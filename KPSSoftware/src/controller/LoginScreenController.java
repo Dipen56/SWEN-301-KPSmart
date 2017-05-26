e8b86949d6a18f13af3f14a8fa4c1878c8cb0950
patevs <ppevans11@gmail.com>
2017-05-26 17:37:24 +1200

    Implemented user login authentication with database

--- KPSSoftware/src/controller/LoginScreenController.java
+++ KPSSoftware/src/controller/LoginScreenController.java
@@ -12,6 +12,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
+import main.KPSDatabase;
+import view.DialogBox;
 
 
 import java.io.IOException;
@@ -22,33 +24,23 @@ public class LoginScreenController {
     public TextField usernameTextfield;
     public PasswordField passwordTextfield;
 
-
     /**
      * this method is connected to the login button as well is the text fields, and is used to perform the steps
      * required for logging into the system.
      */
     @FXML
     public void performLogin(ActionEvent event) throws IOException {
-        // String userName = usernameTextfield.getText();
-        //  String password = passwordTextfield.getText();
-        //System.out.println(userName + " " + password);
-//        if (vaild user){
-//            Parent homescreen = FXMLLoader.load(GUI.class.getResource("/fxml/home screen.fxml"));
-//            Scene homeSecne = new Scene(homescreen);
-//            Stage tempStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-//            tempStage.setScene(homeSecne);
-//            tempStage.show();
-//        }else{
-//            DialogBox.displayMsg("Invaild Input", "Please Check your Username and Password are correct and try again");
-//        }
-        //only here for testing can be deleted later...
-        Parent homescreen = FXMLLoader.load(LoginScreenController.class.getResource("/fxml/home screen.fxml"));
-        Scene homeSecne = new Scene(homescreen);
-        Stage tempStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        tempStage.setScene(homeSecne);
-        tempStage.show();
-
+        String userName = usernameTextfield.getText();
+        String password = passwordTextfield.getText();
+        // checks given user name and password against database
+        if (KPSDatabase.checkLogin(userName,password)){
+            Parent homescreen = FXMLLoader.load(LoginScreenController.class.getResource("/fxml/home screen.fxml"));
+            Scene homeSecne = new Scene(homescreen);
+            Stage tempStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            tempStage.setScene(homeSecne);
+            tempStage.show();
+        }else{
+            DialogBox.displayMsg("Invaild Input", "Please Check your Username and Password are correct and try again");
+        }
     }
-
-
 }

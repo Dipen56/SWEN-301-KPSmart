ee71d71307ae3b47ca66f2868e3f63b66272f8cb
Dipen <pateldipe1@ecs.vuw.ac.nz>
2017-06-04 11:06:36 +1200

    update

--- KPSSoftware/src/model/database/DataPopulater.java
+++ KPSSoftware/src/model/database/DataPopulater.java
@@ -1,13 +1,15 @@
 package model.database;
 
 /**
- * This class is used to populate the initial data, so the programme won't have to be demonstrated with blank data.
+ * This class is used to populate the initial data, so the programme won't have to be demonstrated with blank data. this class is used to load the data need for the program to run.
  *
  * @author Hector
  * @version 2017/5/20
  */
 public class DataPopulater {
 
+
     // TODO: use XML writer to write stuff into xml files
 
+    
 }
